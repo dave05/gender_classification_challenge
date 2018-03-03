a16c168f5cdd9c0d0f82e7a7996702c1f3960999
Dawit21 <dasam2012@gmail.com>
2018-03-02 19:12:18 -0600

    Gender classification four different classifers

--- demo.py
+++ demo.py
@@ -1,7 +1,11 @@
 from sklearn import tree
-
-clf = tree.DecisionTreeClassifier()
-
+from sklearn.naive_bayes import *
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import svm
+clf_TD = tree.DecisionTreeClassifier()
+clf_SVC = svm.SVC()
+clf_gnb= GaussianNB()
+clf_Kneigh = KNeighborsClassifier(n_neighbors=3)
 # CHALLENGE - create 3 more classifiers...
 # 1
 # 2
@@ -17,10 +21,20 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 
 
 # CHALLENGE - ...and train them on our data
-clf = clf.fit(X, Y)
+clf_TD = clf_TD.fit(X, Y)
+clf_gnb = clf_gnb.fit(X, Y)
+clf_Kneigh = clf_Kneigh.fit(X, Y)
+clf_SVC = clf_SVC.fit(X, Y)
 
-prediction = clf.predict([[190, 70, 43]])
+prediction_TD = clf_TD.predict([[190, 70, 43]])
+prediction_gnb = clf_gnb.predict([[190, 70, 43]])
+prediction_Kneigh = clf_Kneigh.predict([[190, 70, 43]])
+prediction_SVC= clf_SVC.predict([[190, 70, 43]])
+
 
 # CHALLENGE compare their reusults and print the best one!
 
-print(prediction)
+print(f"DecisionTreeClassifierprediction:  {prediction_TD}")
+print(f"GaussianNBprediction:  {prediction_gnb}")
+print(f"Knearestprediction:  {prediction_Kneigh}")
+print(f"SVMprediction:  {prediction_SVC}")
